e783ea03ef4aa93a239923be4fccc3edd8f4748c
Mosharraf-Musa <mosharrafny@gmail.com>
2021-03-18 16:26:00 -0400

    context hook in class comp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React, { useState, createContext } from 'react'
+import React, {useState} from 'react'
 import Counter from './Counter'
 import CounterHooks from './CounterHooks'
 
-export const ThemeContext =  React.createContext()
+export const ThemeContext = React.createContext('red')
 
 
 function App() {
   const [theme, setTheme] = useState('red')
+  
   return (
-    <createContext.Provider value = {{backgroundColor: theme}}>
+    <ThemeContext.Provider value ={{backgroundColor: theme}}>
     <p>Welcome to Recipe App</p>
     Counter
     <Counter initialCount={5}/>
     Hook Counter
     <CounterHooks initialCount={3}/>
-    <button onClick={()=>setTheme(prevTheme =>{
-      return prevTheme === 'red'? 'Blue':'red'
+    <button onClick={()=>setTheme(prevTheme=>{
+      return prevTheme === 'red'? 'blue': 'red'
     })}>Toggle Theme</button>
-    </createContext.Provider>
+    </ThemeContext.Provider>
     
   );
 }
