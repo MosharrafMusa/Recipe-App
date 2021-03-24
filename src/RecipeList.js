6033c3235db4be99824d9ff11e165d0d52180a4e
Mosharraf-Musa <mosharrafny@gmail.com>
2021-03-24 15:29:15 -0400

    RecipeList added

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import Recipe from './Recipe'
 function RecipeList() {
     return (
-        <div>
-            
-        </div>
+        <>
+          <Recipe/>  
+          <Recipe/>  
+        </>
     )
 }
 
