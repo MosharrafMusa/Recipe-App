7a354bcd10a3c8293434e5a89a60882d7345d1b3
Mosharraf-Musa <mosharrafny@gmail.com>
2021-03-24 15:31:29 -0400

    clean up app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import React, {useState} from 'react'
-import Counter from './Counter'
-import CounterHooks from './CounterHooks'
-
-export const ThemeContext = React.createContext('green')
-
+import React from 'react'
 
 function App() {
-  const [theme, setTheme] = useState('green')
+  
   
   return (
-    <ThemeContext.Provider value ={{backgroundColor: theme}}>
-    <p>Welcome to Recipe App</p>
-    Counter
-    <Counter initialCount={5}/>
-    Hook Counter
-    <CounterHooks initialCount={3}/>
-    <button onClick={()=>setTheme(prevTheme=>{
-      return prevTheme === 'red'? 'blue': 'red'
-    })}>Toggle Theme</button>
-    </ThemeContext.Provider>
-    
+    null
   );
 }
 
